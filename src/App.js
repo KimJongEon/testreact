5d0a0e8668d331f893889a95b0fb33eb615f8026
KimJongEon <kjo4134@gmail.com>
2022-06-02 10:03:39 +0900

    버튼에 이벤트 리스너(핸들러) 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-// import logo from './logo.svg';
-// import son from './son.jpeg'
+/* eslint-disable */ // waring 제거 (오류는 아님)
+import logo from './logo.svg';
+import son from './son.jpeg'
 import React, { useState } from 'react';
 import './App.css';
 
@@ -7,10 +8,14 @@ import './App.css';
 function App() {
 
 
+
 // ES6 destructuring 문법
 //state는 변수 대신 쓰는 데이터 저장공간, useState()를 이용해 만들어야함
 //state에 데이터를 저장하는 이유 : HTML이 자동으로 재렌더링, HTML이 새로고침 없이도 변경됨
 let [글제목, 글제목변경] = useState(['남자 코트 추천', '신발 추천', '강남 우동 맛집']);
+let likeCnt = 0;
+let [하트, 하트변경] = useState(likeCnt);
+
 // posts라는 변수에 데이터를 저장
 let posts = '맛집 리스트';
 // let mainStyle = {color : 'blue' , fontSize : '100px'} ;
@@ -22,7 +27,7 @@ let posts = '맛집 리스트';
       </div>
       <div className="list">
         {/* posts라는 변수를 통해 데이터 바인딩 */}
-        <h3> { 글제목[0] + '[글 제목 배열 0]' } </h3>
+        <h3> { 글제목[0] + '[글 제목 배열 0]' } <span onClick={ ()=>{ 하트변경(하트 + 1)} }>❤️</span> {하트} </h3>
         <p>글 내용</p>
         <hr/>
       </div>
