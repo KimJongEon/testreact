8fc6700e2f788c89cafd8929742711611e866666
KimJongEon <kjo4134@gmail.com>
2022-06-02 09:40:50 +0900

    useState 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 // import logo from './logo.svg';
 // import son from './son.jpeg'
+import React, { useState } from 'react';
 import './App.css';
 
 
 function App() {
 
+
+// ES6 destructuring 문법
+//state는 변수 대신 쓰는 데이터 저장공간, useState()를 이용해 만들어야함
+//state에 데이터를 저장하는 이유 : HTML이 자동으로 재렌더링, HTML이 새로고침 없이도 변경됨
+let [postTitle_01, 글제목변경] = useState(['남자 코트 추천']);
+let [postTitle_02, 글제목변경2] =useState(['신발 추천']);
+let [postTitle_03, 글제목변경3] = useState([ '강남 우동 맛집']);
 // posts라는 변수에 데이터를 저장
 let posts = '맛집 리스트';
 // let mainStyle = {color : 'blue' , fontSize : '100px'} ;
@@ -16,17 +24,23 @@ let posts = '맛집 리스트';
       </div>
       <div className="list">
         {/* posts라는 변수를 통해 데이터 바인딩 */}
-        <h3> { posts } </h3>
-        <p>5월 30일 발행</p>
-        깃허브 테스트
+        <h3> { postTitle_01[0] + '[글 제목 1]' } </h3>
+        <p>글 내용</p>
         <hr/>
       </div>
 
       <div className="list">
-        <h3> { posts } </h3>
-        <p>5월 30일 발행</p>
+        <h3> { postTitle_02[0] +'[글 제목 2]' } </h3>
+        <p>글 내용</p>
         <hr/>
       </div>
+
+      <div className="list">
+        <h3> { postTitle_03[0] + '[글 제목 3]'} </h3>
+        <p>글 내용</p>
+        <hr/>
+      </div>
+
       {/* <div style= { mainStyle } > css 테스트</div>
       <img src= { logo } alt = '로고이미지'/>
       <img src= { son } alt = '손 이미지'/> */}
